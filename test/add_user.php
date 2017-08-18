7ffccb4e9878e87c8e80909aa09ddbe35bc1d2b9
arul <whatsappmail8@gmail.com>
2017-08-18 21:45:15 +0530

    just backup

--- test/add_user.php
+++ test/add_user.php
@@ -1,9 +1,6 @@
 <?php
 
-/* 
- *  This is created by ARULKUMR S, THE AUTHOR
 
- */
 
 $link = mysqli_connect("127.0.0.1", "root", "", "fb2");
 session_start();
@@ -13,29 +10,40 @@ session_start();
             $userid=$_SESSION['id'];
             $add_user=$_POST['add_user'];
             
-$sql = "CREATE TABLE $userid".
+$sql3 = "CREATE TABLE $userid".
         "_$add_user (no INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, sender_id VARCHAR(10),receiver_id".
                     " VARCHAR(10),message TEXT,time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,".
                     "status INT(3))";
             
-                            $res=mysqli_query($link, $sql);
+                            $res3=mysqli_query($link, $sql3);
                             
-                            if (!$res)    die("FFF".mysqli_error($link));
+                            if (!$res3)    die("FFF".mysqli_error($link));
 
                             
-$sql2 = "CREATE TRIGGER update_to_$userid
+$sql4 = "CREATE TRIGGER update_to_$userid
         AFTER INSERT ON $userid"."_$add_user FOR EACH ROW BEGIN
         IF NEW.receiver_id=$userid THEN
         INSERT INTO $userid"."_incoming_message (sender_id,message,time) VALUES
-        (NEW.sender_id,NEW.message,NEW.time,'101'); END IF;END;";    
+        (NEW.sender_id,NEW.message,NEW.time,'101');
+        ELSEIF NEW.receiver_id=$add_user THEN
+        INSERT INTO $add_user"."_incoming_message (sender_id,message,time) VALUES
+            (NEW.sender_id,NEW.message,NEW.time,'101');
+ END IF;END;";    
         
-            $res2=mysqli_query($link, $sql2);
+            $res4=mysqli_query($link, $sql4);
                             
-                            if (!$res2)    die("adding_user".mysqli_error($link));
+                            if (!$res4)    die("adding_user".mysqli_error($link));
 
+  $sql1="SELECT no FROM $userid"."_chatlist WHERE user_id=$add_user";                          
+                            
+  $sql2="INSERT INTO $userid"."_chatlist (user_id) VALUES ('$add_user')";                                      
                                         
-                                        
+            $res1 = mysqli_query($link, $sql1);
             
-                                        
+            if(!$res1) die("error".mysqli_error ($link));
+                    
+                               $row= mysqli_fetch_array($res3,MYSQLI_ASSOC);
+                               if(!row) {       die("eror operation");               }
+                               
             
 }
